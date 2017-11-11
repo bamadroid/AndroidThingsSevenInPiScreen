eccffacceaf384611900ac56db0523ed3348809a
bamadroid <bama.android@gmail.com>
2017-11-10 21:30:57 -0600

    Added Buttons to verify onClick

--- app/src/main/java/com/example/rollt/androidthings7inpiscreen/MainActivity.java
+++ app/src/main/java/com/example/rollt/androidthings7inpiscreen/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.rollt.androidthings7inpiscreen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 /**
  * Skeleton of an Android Things activity.
@@ -24,11 +30,67 @@ import android.os.Bundle;
  */
 public class MainActivity extends Activity
 {
+    private Button mButton;
+    private TextView mTextView;
+    private Button mPowerButton;
+    private ProgressBar mSpinner;
+    private static int sCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.testText);
+
+        mSpinner = findViewById(R.id.progressBar1);
+
+        mButton = findViewById(R.id.testButton);
+        mButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                // Update text and display click count
+                mTextView.setText(getString(R.string.its_working_text, ++sCount));
+            }
+        });
+
+        mPowerButton = findViewById(R.id.powerButton);
+        mPowerButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Show progress spinner
+                mSpinner.setVisibility(View.VISIBLE);
+
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        // Start Power Off
+        powerOff();
+    }
+
+    /**
+     * Safely Power Down Raspberry Pi
+     */
+    private void powerOff()
+    {
+        try
+        {
+            Runtime.getRuntime().exec("reboot -p");
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
