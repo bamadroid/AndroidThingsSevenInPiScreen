764334850026caf3bff84262a334fa149ecc77ed
bamadroid <bama.android@gmail.com>
2017-11-20 20:04:51 -0600

    Added power down confirmation

--- app/src/main/java/com/example/rollt/androidthings7inpiscreen/MainActivity.java
+++ app/src/main/java/com/example/rollt/androidthings7inpiscreen/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.rollt.androidthings7inpiscreen;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,8 @@ public class MainActivity extends Activity
     private Button mButton;
     private TextView mTextView;
     private Button mPowerButton;
+    private Button mClearButton;
+    private Button mBigJumpButton;
     private ProgressBar mSpinner;
     private static int sCount = 0;
 
@@ -44,17 +48,50 @@ public class MainActivity extends Activity
             }
         });
 
+        mClearButton = findViewById(R.id.clearButton);
+        mClearButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                // Update text and display click count
+                sCount = 0;
+                mTextView.setText(getString(R.string.its_working_text, ++sCount));
+            }
+        });
+
+        mBigJumpButton = findViewById(R.id.bigJumpButton);
+        mBigJumpButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                // Update text and display click count
+                sCount += 100;
+                mTextView.setText(getString(R.string.its_working_text, sCount));
+            }
+        });
+
         mPowerButton = findViewById(R.id.powerButton);
         mPowerButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                // Show progress spinner
-                mSpinner.setVisibility(View.VISIBLE);
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Power Down Device?")
+                        .setMessage("Do you really want to power off Things?")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
 
-                // Destroys the Activity
-                finish();
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // Show progress spinner
+                                mSpinner.setVisibility(View.VISIBLE);
+
+                                // Destroys the Activity
+                                finish();
+                            }})
+                        .setNegativeButton(android.R.string.no, null).show();
             }
         });
     }
